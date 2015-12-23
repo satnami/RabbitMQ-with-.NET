9d24fd70e9b9241c9e216ba08ad478e7ca2e581c
Sami Almouhtaseb <mssatnami@gmail.com>
2015-12-23 23:13:55 +0800

    initial commit

---
+++ RabbitMQ/RabbitMQ.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.Console
+{
+    class Program
+    {
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string HostName = "localhost";
+
+        static void Main(string[] args)
+        {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                Password = Password,
+                UserName = UserName ,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var model = connection.CreateModel();
+
+            model.
+        }
+    }
+}
